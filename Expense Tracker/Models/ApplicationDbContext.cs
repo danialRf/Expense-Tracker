4262d6871721ce6b2f3db3b43918a6232377f27a
Danial <D.roshanfekr@gmial.com>
2023-10-18 18:50:38 +0330

    initial of database

---
+++ Expense Tracker/Models/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Expense_Tracker.Models
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base (options) 
+        {
+            
+        }
+
+        DbSet<Transaction> Transactions {  get; set; }
+        DbSet<Category> Categories { get; set; }
+
+
+    }
+}
---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int Amount { get; set; }
+        public string? Note { get; set; }
+        public DateTime DateTime { get; set; } = DateTime.Now;
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+    }
+}
